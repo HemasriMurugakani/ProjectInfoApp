9c34ec120b5777539b92a5a664c4a1da74b9da61
HemasriMurugakani <hemaleena1102@gmail.com>
2024-10-26 16:26:37 +0530

    Zustand for logexpense completed

--- src/components/LogExpense.jsx
+++ src/components/LogExpense.jsx
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, FlatList } from 'react-native';
 import Svg, { Circle, Path } from 'react-native-svg';
-import { useSelector, useDispatch } from 'react-redux';
 import ExpenseBottomSheet from './ExpenseModal';
-import { addExpense } from '../../redux/expenseSlice';
+import useExpenseStore from '../zustand/expenseStore';
 
 const AddIcon = () => (
   <Svg width="24" height="24" viewBox="0 0 24 24" fill="none">
@@ -20,15 +19,13 @@ const AddIcon = () => (
 
 const LogExpense = () => {
   const [isBottomSheetVisible, setBottomSheetVisible] = useState(false);
-  const expenses = useSelector(state => state.expenses);
-  const dispatch = useDispatch();
-
+  const { expenses, addExpense } = useExpenseStore(); 
   const toggleBottomSheet = () => {
     setBottomSheetVisible(!isBottomSheetVisible);
   };
 
   const handleExpenseSubmit = (expense) => {
-    dispatch(addExpense(expense));
+    addExpense(expense); 
     toggleBottomSheet();
   };
 
@@ -50,7 +47,7 @@ const LogExpense = () => {
         renderItem={({ item }) => (
           <View style={styles.expenseRow}>
             <Text style={styles.expenseName}>{item.name}</Text>
-            <Text style={styles.expenseDate}>• {item.date.toLocaleDateString()} • {item.amount} {item.currency}</Text>
+            <Text style={styles.expenseDate}>• {new Date(item.date).toLocaleDateString()} • {item.amount} {item.currency}</Text>
             {item.file && (
               <Text style={styles.fileName}>File: {item.file.name}</Text>
             )}
@@ -121,4 +118,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default LogExpense;
+export default LogExpense;  
